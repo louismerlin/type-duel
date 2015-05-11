e10c5b1136c3feb633f086b9df9d7f2f8f0591c2
louismerlin <louismerlin@github.com>
2015-05-11 08:57:28 +0200

    fixed beginning, added spaces

--- js/online.js
+++ js/online.js
@@ -43,7 +43,6 @@ function hide(){
   document.getElementById("menu").style.display = "none";
   document.getElementById("title").style.display = "none";
   document.getElementById("game").style.display = "initial";
-  document.getElementById("gameInput").focus();
 }
 
 function onReceive(data) {
@@ -53,7 +52,7 @@ function onReceive(data) {
       input.style.display = "none";
     } else if (data == "$1"){
         turn = 3;
-        input.style.backgroundColor = "grey";
+        output.innerHTML = "You go first";
     } else if (data == "$11") {
         players[0].me = false;
         players[1].me = true;
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-var input = document.getElementById('gameInput'),
-    inputString = "",
+var inputString = "",
     output = document.getElementById('gameOutput'),
+    firstPush = true,
     turn,
     players = [],
     thisPlayer,
@@ -8,32 +8,34 @@ var input = document.getElementById('gameInput'),
     aftW = "",
     otherPlayer,
     keyCodes = [],
-    alphabet = ['a','b','c','d','e','f','h','i','j','k','l','m','n','o','p','q',
-    'r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J',
-    'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' '],
+    alphabet = [],
     keys = [];
 
-input.value = "";
 output.innerHTML = "";
+for(i=65;i<91;i++){
+  alphabet.push(i);
+}
+alphabet.push(32);
 
 document.body.addEventListener("keydown", function(e) {
     keyCodes[e.keyCode] = true;
-    key = e.key;
     if (keyCodes[13]) { // ENTER
-      if(turn == 3){
-        conn.send(input.value);
-        input.style.display = "none";
-        input.value = "";
-        inputString = "";
-      } else if (turn == 1) {
+      if(turn == 3 || turn == 1){
         endTurn();
       }
     } else if (keyCodes[27]) { // ESCAPE
         // PAUSE GAME (NO MORE "game", show "pause")
-    } else if (alphabet.indexOf(key)!=-1 && turn != 3 && inputString.length<21){
-      inputString+=key;
+    } else if (alphabet.indexOf(e.keyCode)!=-1 && turn != 3 && inputString.length<21){
+      inputString+=String.fromCharCode(e.keyCode).toLowerCase();
       i = "l" + (inputString.length-1);
       document.getElementById(i).style.color = "green";
+    } else if (alphabet.indexOf(e.keyCode)!=-1 && turn == 3 && inputString.length<21){
+      if(firstPush){
+        output.innerHTML = "";
+        firstPush = false;
+      }
+      inputString+=String.fromCharCode(e.keyCode).toLowerCase();
+      output.innerHTML+=String.fromCharCode(e.keyCode).toLowerCase();
     }
     keyCodes[e.keyCode] = false;
 });
@@ -52,7 +54,7 @@ function initiateGame() {
         if (Math.floor(Math.random() * 2) == 1) { // 1/2
             conn.send("$0");
             turn = "3";
-            input.style.backgroundColor = "grey";
+            output.innerHTML = "You go first";
             players[0].me = true;
             players[1].me = false;
             thisPlayer = players[0];
@@ -61,7 +63,6 @@ function initiateGame() {
         } else {
             conn.send("$1");
             turn = "0";
-            input.style.display = "none";
             players[0].me = false;
             players[1].me = true;
             thisPlayer = players[1];
@@ -69,6 +70,7 @@ function initiateGame() {
             conn.send("$10");
         }
     }
+    inputString = "";
 }
 
 function endTurn(){
